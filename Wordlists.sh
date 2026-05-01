94c68fc55addc6e5d6a1403dd71ddf3046ece1a9
Alice Maria <72365221+Alice16021@users.noreply.github.com>
2026-05-01 10:02:41 -0300

    Create Wordlists.sh for wordlist generation
    Add Wordlists.sh script for generating numeric and string wordlists

---
+++ Wordlists.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+crunch 4 6 1234567890 -o /root/numerica 6-8.txt  #Wordlist com int de 4 a 6 numeros#
+crunch 9 9 1234567890 -T maria@@@@ -o (diretório) #Wordlist de strings começando com "maria" seguido de 4 números aleatórios#
